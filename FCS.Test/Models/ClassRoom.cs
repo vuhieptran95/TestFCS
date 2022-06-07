226bb8e072a02a848149dc0a48d2b6e231e67462
hiep.tran <vuhiep.tran95@gmail.com>
2022-06-07 16:14:36 +0700

    First commit

---
+++ FCS.Test/Models/ClassRoom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace FCS.Test.Models
+{
+    public class ClassRoom
+    {
+        public ClassRoom()
+        {
+            Name = "Math class";
+            Students = new List<Student>
+            {
+                new Student("1", "Iron Man"),
+                new Student("2", "Batman"),
+                new Student("3", "Superman")
+            };
+            var x = new Dictionary<string, string>()
+            {
+                {"12", "!2"}
+            };
+        }
+            
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+            
+    }
+
+    public class Student
+    {
+        public Student(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
